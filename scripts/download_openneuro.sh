cc02dd45f5b808c45c6c758905395a9a63dc16fa
Akshay Rao <akshaysrao1@gmail.com>
2025-10-13 15:50:27 -0700

    Revert "Reinitialize silentSpeech with merged speechdecoding code and data validation utilities"
    This reverts commit 36523f98fc117afe1416611113c27a01966141ea.

--- scripts/download_openneuro.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-echo "[!] For OpenNeuro ds004196, prefer python tool: python -m src.scripts.fetch_openneuro --dataset ds004196 --subjects sub-01 sub-02"
--- src/models/decoders.py
+++
@@ -1,7 +0,0 @@
-import torch.nn as nn
-class CTCHead(nn.Module):
-    def __init__(self, in_dim: int, vocab_size: int):
-        super().__init__()
-        self.proj = nn.Linear(in_dim, vocab_size)
-    def forward(self, x):
-        return self.proj(x)
